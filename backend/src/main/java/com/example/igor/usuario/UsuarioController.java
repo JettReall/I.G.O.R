04c14dd13f832ddfc3bcb10cc5f3bedbe758b08a
Guilherme Vall <seuemail@email.comguilherme.felizardo@icloud.com>
2026-06-04 17:08:19 -0300

    correcao

--- backend/src/main/java/com/example/igor/usuario/UsuarioController.java
+++ backend/src/main/java/com/example/igor/usuario/UsuarioController.java
@@ -3,16 +3,10 @@ package com.example.igor.usuario;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
