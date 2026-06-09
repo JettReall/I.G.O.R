b781ef8bf754b44acb482993dd12c1bbe20f5130
MiloOliveira <miloliveira143@gmail.com>
2026-06-08 23:32:37 -0300

    Criado elementos da tela de ficha

--- frontend/IGOR/src/componentes/ficha/componentes.jsx
+++ frontend/IGOR/src/componentes/ficha/componentes.jsx
@@ -52,8 +52,84 @@ function Atributos() {
   );
 }
 
-function StatusContainer({vidaAtual, vidaMaxima}) {
+function Status({nome,valor,lado, isDadoUnico}) {
+     let  classeDupla = "dupla"
+
+     if(!lado) {
+          lado = "up"
+     }
+
+     if (isDadoUnico === true) {
+          classeDupla = "solo"
+     }
+
+     return (
+     
+     <div className={clsx(estilos['container-menor'],estilos[lado],estilos[classeDupla])}>
+          <p className={estilos['nome-status']}>{nome}</p>
+          <strong className={estilos['valor-status']}>{valor}</strong>
+     </div>
+     )
+}
+
+function StatusContainer({nome_up, valor_up, nome_down, valor_down,isDadoUnico}) {
+
+     if (!isDadoUnico) {
+          return (
+               <div className={clsx(estilos["container-status"])}>
+                    <Status valor={valor_up} nome={nome_up} lado={"up"} isDadoUnico={isDadoUnico}/>
+                    <Status valor={valor_down} nome={nome_down} lado={"down"}/>
+               </div>
+          )
+     }  else {
+          return (
+               <div className={clsx(estilos["container-status"])}>
+                    <Status valor={valor_up} nome={nome_up} lado={"up"} isDadoUnico={isDadoUnico}/>
+               </div>
+          )
+     }
+}
+
+function Pericia({ dados, isHeader}) {   // ← recebe um objeto chamado "dados"
+     let classeHeader = "";
+     if (isHeader) {
+          classeHeader = "header";
+     } else {
+          classeHeader = ""
+     }
+
+  return (
+    <div className={clsx(estilos['elemento-container'],estilos[classeHeader])}>
+      <strong className={estilos['pericia-nome']}>{dados.nome}</strong>
+      <strong className={estilos['pericia-atributo']}>{dados.atributo}</strong>
+      <strong className={estilos['pericia-treino']}>{dados.treino}</strong>
+      <strong className={estilos['pericia-bonus']}>{dados.bonus}</strong>
+      <strong className={estilos['pericia-extra']}>{dados.extra}</strong>
+      <strong className={estilos['pericia-total']}>{dados.total}</strong>
+    </div>
+  );
+}
+
+function AtaqueRitual({dados_ataque_ritual, isHeader}) {
+
+          let classeHeader = "";
+     if (isHeader) {
+          classeHeader = "header";
+     } else {
+          classeHeader = ""
+     }
+     return (
+          <div className={clsx(estilos['elemento-container'],estilos[classeHeader])}>
+               <strong className={estilos['ataque-ritual-nome']}>{dados_ataque_ritual.nome}</strong>
+               <strong className={estilos['ataque-ritual-categoria']}>{dados_ataque_ritual.categoria}</strong>
+               <strong className={estilos['ataque-ritual-tipo']}>{dados_ataque_ritual.tipo}</strong>
+               <button className={estilos['informacao-ataque-ritutal']}>
+                    <img src="..\..\assets\imagens\icones\simbolo-de-informacao.png" alt="" />
+               </button>
+          </div>
+     )
 
 }
 
-export {Atributos}
+
+export {Atributos, StatusContainer, Pericia, AtaqueRitual}
--- frontend/IGOR/src/paginas/ficha/ficha.jsx
+++ frontend/IGOR/src/paginas/ficha/ficha.jsx
@@ -4,13 +4,122 @@ import clsx from "clsx";
 
 import { HeaderBase } from "../../componentes/header/headers"
 import { BotaoRetorno } from "../../componentes/botoes/Botoes"
-import { Atributos } from "../../componentes/ficha/componentes";
+import { StatusContainer, Atributos, Pericia, AtaqueRitual } from "../../componentes/ficha/componentes";
 import estilos from './ficha.module.css'
 
 
 //className={estilos['corpo']}
 
+
+
+function ContainerPericias() {
+     
+
+const periciaExemplo = {
+  nome: "Prestidigitação",
+  atributo: "FOR",
+  treino: 5,
+  bonus: 2,
+  extra: 0,
+  total: 4   // normalmente calculado: (treino ? 2 : 0) + bonus + extra
+};
+
+
+const periciaExemplo2 = {
+  nome: "Nome",
+  atributo: "Atributo",
+  treino: "Treino",
+  bonus: "Bônus",
+  extra: "Extra",
+  total: "Total"  // normalmente calculado: (treino ? 2 : 0) + bonus + extra 
+};
+
+
+     return (
+          <div className={estilos['container-pericias']}>
+               <Pericia dados={periciaExemplo2} isHeader={true}></Pericia>
+               <Pericia dados={periciaExemplo}></Pericia>
+               <Pericia dados={periciaExemplo}></Pericia>
+
+          </div>
+     )
+     
+}
+
+function ContainerAtaques() {
+
+     const HeaderAtaque = {
+     nome: "Nome",
+     categoria: "Atributo Base",
+     tipo: "Tipo de Ataque",
+     }
+
+     return (
+          <div className="">
+               <AtaqueRitual dados_ataque_ritual={HeaderAtaque} isHeader={true}/>
+          </div>
+     )
+
+
+}
+
+function ContainerRituais() {
+
+     const HeaderRitual = {
+     nome: "Ritual",
+     categoria: "Criculo",
+     tipo: "Elemento",
+     }
+
+     return (
+          <AtaqueRitual dados_ataque_ritual={HeaderRitual} isHeader={true}/>
+     )
+
+}
+
+
+
+function ContainerAtaqueRitual() {
+
+  const [isRitual, setIsRitual] = useState(true);   
+  const [abaRitual, setAbaRitual] = useState(true);
+  const [abaAtaque, setAbaAtaque] = useState(false);
+
+  const TrocaTela = () => {
+    setIsRitual(!isRitual);    // alterna entre true/false
+    setAbaRitual(!abaRitual);
+    setAbaAtaque(!abaAtaque);
+}
+
+     const BotoesAtaqueRitual = () => {
+          return (
+               <div className={estilos['container-botao-atk-ritual']}>
+                    <button className={estilos['botao-ataque-ritual']} disabled={abaRitual} onClick={TrocaTela}>Rituais</button>
+                    <button className={estilos['botao-ataque-ritual']} disabled={abaAtaque} onClick={TrocaTela}>Ataques</button>
+               </div>
+          )
+     }
+
+////---------------------------------------------------------------
+     if (isRitual) {
+          return (
+               <div className={estilos['container-ataque-ritual']}>
+                    <BotoesAtaqueRitual/>
+                    <ContainerRituais />
+               </div>
+          )
+     } else {
+          return (
+               <div className={estilos['container-ataque-ritual']}>
+                    <BotoesAtaqueRitual/>
+                    <ContainerAtaques/>
+               </div>
+          )
+     }
+}
+
 function Ficha() {
+
      return (
           <>
           <header>
@@ -18,6 +127,12 @@ function Ficha() {
           </header>
           <main className={estilos.corpo}>
                <Atributos/>
+               <div>
+                    <StatusContainer valor_up={3210} valor_down={210} nome_down={"HP MAX"} nome_up={"DEFESA"}/>
+                    <StatusContainer valor_up={245} nome_up={"Ataque"} isDadoUnico={true}/>
+               </div>
+               <ContainerPericias/>
+               <ContainerAtaqueRitual/>
           </main>
      
           </>
