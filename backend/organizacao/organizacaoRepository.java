48f4f6135180db36126bd85ea35242184c8a23b1
hosanafg <fernandeshosana48@gmail.com>
2026-06-10 20:38:34 -0300

    Remove arquivos rastreados que estão no .gitignore

--- backend/organizacao/organizacaoRepository.java
+++
@@ -1,9 +0,0 @@
-//organizacaoRepository.java
-
-package com.example.demo.organizacao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface organizacaoRepository extends JpaRepository<organizacaoEntity, Long> {
-
-}
--- backend/organizacao/organizacaoUtil/organizacaoUtil.java
+++
@@ -1,7 +0,0 @@
-//organizacaoUtil.java
-
-package com.example.demo.organizacao.organizacaoUtil;
-
-public class organizacaoUtil {
-
-}
