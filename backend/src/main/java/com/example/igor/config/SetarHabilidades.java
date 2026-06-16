b08898a9b65f9f62a26fbc7c6721914db78690d7
HagiSL <leonelhagig@gmail.com>
2026-06-16 20:12:38 -0300

    Trilha e Habilidades

--- backend/src/main/java/com/example/igor/config/SetarHabilidades.java
+++ backend/src/main/java/com/example/igor/config/SetarHabilidades.java
@@ -7,18 +7,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.core.annotation.Order;
 
 import java.io.InputStream;
 import java.util.List;
 
 @Component
+@Order(1)
 public class SetarHabilidades implements CommandLineRunner {
 
     @Autowired
     private HabilidadeRepository habilidadeRepository;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper(); //erro: "required a bean of type" se usar autowired
 
     @Override
     public void run(String... args) throws Exception {
