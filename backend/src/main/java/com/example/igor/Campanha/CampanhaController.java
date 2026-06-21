3394721a291180dc8c91324d3dc76e90ca03104d
hosanafg <fernandeshosana48@gmail.com>
2026-06-21 19:55:49 -0300

    ??

--- backend/src/main/java/com/example/igor/Campanha/CampanhaController.java
+++ backend/src/main/java/com/example/igor/Campanha/CampanhaController.java
@@ -1,14 +1,23 @@
 package com.example.igor.Campanha;
 
-import com.example.igor.Campanha.DTO.CampanhaCombateDTO;
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.igor.Campanha.DTO.CampanhaCombateDTO;
+
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5176")
 @RequestMapping("/campanha")
 public class CampanhaController {
     @Autowired
